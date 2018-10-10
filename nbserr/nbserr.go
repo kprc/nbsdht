23448cfb6f87ed7e4b60a282aee6c9e5e7246a64
rickey.liao <rickey@rickey.liao's macbook>
2018-10-10 12:18:35 +0800

    add to repo

---
+++ nbserr/nbserr.go
@@ -0,0 +1,22 @@
+package nbserr
+
+const (
+	REGERR = 101
+	EXE_NOHANDLE = 201
+	SERVER_RESOLVE_UDP_ERR=301
+	LISTEN_UDP_ERR=302
+	LISTEN_UDP_RESTART=303
+	UDP_MESSAGE_LENGTH_ERR=304
+	ERROR_DEFAULT=99999
+)
+
+
+type NbsErr struct {
+	Errmsg string
+	ErrId uint32
+}
+
+
+func (ne NbsErr)Error() string{
+	return ne.Errmsg
+}
